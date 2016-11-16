8f161c543b960df5aae418731230a23f6dd7478f
Myxfall <maximilienromain96@hotmail.com>
2016-11-16 15:21:48 +0100

    Adding marquers and addresses

--- dashboard.R
+++ dashboard.R
@@ -5,6 +5,7 @@ setwd("/Users/user/Documents/3eme/MOBI-AID/MOBI-AID/")
 library(shiny)
 library(shinydashboard)
 library(leaflet)
+library("RSQLite")
 
 ui <- dashboardPage(
   dashboardHeader(title = "MOBIAID Dashboard"),
@@ -32,7 +33,23 @@ server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
   
-  map = leaflet() %>% addTiles() %>% setView(4.350382, 50.847436, zoom = 12)
+  con <- dbConnect(SQLite(), dbname="mobilityBike.db")
+  query <- "SELECT longitude FROM StaticTable"
+  long <- unlist(dbGetQuery(con, query))
+  query <- "SELECT latitude FROM StaticTable"
+  lat <- unlist(dbGetQuery(con, query))
+  query <- "SELECT address FROM StaticTable"
+  add <- as.vector(unlist(dbGetQuery(con, query)))
+  print(typeof(add))
+  
+  mat <- matrix(c(long,lat), ncol = 2)
+  try <- c("BONJOUR", "ALLO", "STANDARD")
+  #Forming the matrix of station's positions
+  a <- c(4.334831,4.352776,4.346942)
+  b <- c(50.89752,50.85192,50.84563)
+  c <- matrix(c(a,b), ncol = 2)
+  
+  map = leaflet() %>% addTiles() %>% setView(4.350382, 50.847436, zoom = 13) %>% addMarkers(data = mat, popup = add)
   output$plot1 = renderLeaflet(map)
 }
 
