df383733079b2f2f01be972d8f017ea6535c79f1
Myxfall <maximilienromain96@hotmail.com>
2017-02-16 15:00:38 +0100

    script for the small database

---
+++ oldDatasDeletingForOneWeek.R
@@ -0,0 +1,23 @@
+#!/usr/bin/Rscript
+setwd("/Users/user/Documents/3eme/MOBI-AID/MOBI-AID/")
+
+library("dplyr")
+library("RSQLite")
+
+#Take the actually epoch timestamp
+timestamp <- as.POSIXct( Sys.time() )
+#Convert in INTEGER
+timestamp <- as.integer( timestamp )
+#Substract 604800 sec on the timestamp to have the timeStamp one week ago
+timeStampWeekAgo <- ( timestamp - 604800 ) * 1000
+
+#Connect to the dataBase
+con <- dbConnect(SQLite(), dbname="mobilityBike_oneWeek.db")
+
+#Create the query of deleting old datas
+query <- paste("DELETE FROM dynamicTable WHERE timeStamp < ", timeStampWeekAgo)
+
+#Send Query
+dbSendQuery(con, query)
+
+dbDisconnect(con)
