d3955493060e8b34132d48629a1b14c11479f354
Myxfall <maximilienromain96@hotmail.com>
2017-02-23 14:42:32 +0100

    clustering plot

--- clustering.R
+++ clustering.R
@@ -4,7 +4,9 @@ setwd("/Users/user/Documents/3eme/MOBI-AID/MOBI_New/MOBI-AID")
 library("dplyr")
 library("RSQLite")
 library("ape")
-
+library("rafalib")
+library("pvclust")
+library("dendextend")
 
 #Connection to database
 con <- dbConnect(SQLite(), dbname="mobilityBike_oneWeek.db")
@@ -37,9 +39,20 @@ for (i in 1:nrow(tmpDataFrame)) {
 
 distEucl <- dist(as.matrix(stationDataFrame))
 hc <- hclust(distEucl)
-plot(as.dendrogram(hc), hang = -1, cex = 0.3, xlab = "Cluster", horiz = TRUE)
+
+dend <- as.dendrogram(hc)
+dend <- rotate(dend, 1:150)
+dend <- color_branches(dend, k=20)
+dend <- set(dend, "labels_cex", 0.1)
+plot(dend)
+
+
+#plot(as.dendrogram(hc), hang = -1, cex = 0.3, xlab = "Cluster", horiz = TRUE)
 #plot(as.phylo(hc), type = "fan", cex = 0.3)
+#plot(hc,cex=0.5)
+#myplclust(hc, cex=0.5)
 
-
+#result <- pvclust(as.matrix(stationDataFrame))
+#plot(result)
 
 
